35152ade8da4b0b07be170a00abc289d2ff5ff7f
Christopher Durtschi <christopher.durtschi@gmail.com>
2012-07-09 16:04:21 -0600

    Fill in code for Moodle::Converter

--- vendor/plugins/moodle2cc/lib/moodle/converter.rb
+++ vendor/plugins/moodle2cc/lib/moodle/converter.rb
@@ -6,30 +6,17 @@ module Moodle
 
     # This is called by the worker to
     def export(to_export = Canvas::Migration::Migrator::SCRAPE_ALL_HASH)
-      #You can completely ignore to_export
-
-      #todo convert the uploaded .zip file from a moodle file to a canvas-cc file
-      # there is a reference to the .zip file: @archive_file
-      # or you can:
       #unzip_archive
-      moodle_backup = Moodle2CC::Moodle::Backup.parse @archive_file
-      converter = Moodle2CC::CC::CanvasConverter.new moodle_backup, export_dir
-      converter.convert
+      migrator = Moodle2CC::Migrator.new @archive_file.path, @unzipped_file_path
+      migrator.migrate
 
-      # and use @unzipped_file_path as a reference to the package unzipped
-      # This all happens in the parent class Canvas::Migration::Migrator
-
-
-      #todo now that you have a canvas-cc formatted .zip file...
-      # It may be as simple as:
-      # If not we'll think about it some more later. :)
-      @settings[:archive_file] = converter.converted_file
+      @settings[:archive_file] = File.open(migrator.imscc_path)
       cc_converter = CC::Importer::Canvas::Converter.new(@settings)
       cc_converter.export
       @course = cc_converter.course # the @course needs to be the return value
 
     ensure
-      # cleanup of processing files...
+      FileUtils.rm migrator.imscc_path if File.exists?(migrator.imscc_path)
     end
 
   end
--- vendor/plugins/moodle2cc/spec_canvas/moodle_converter_spec.rb
+++ vendor/plugins/moodle2cc/spec_canvas/moodle_converter_spec.rb
@@ -3,11 +3,11 @@ require File.dirname(__FILE__) + '/spec_helper'
 describe Moodle::Converter do
 
   before(:all) do
-    fixture_dir = require File.dirname(__FILE__) + '/fixtures'
-    archive_file_path = File.join(fixture_dir, 'non_existent_moodle_1_9.zip')
+    fixture_dir = File.dirname(__FILE__) + '/fixtures'
+    archive_file_path = File.join(fixture_dir, 'moodle_backup_1_9.zip')
     unzipped_file_path = File.join(File.dirname(archive_file_path), "moodle_#{File.basename(archive_file_path, '.zip')}", 'oi')
     convert_folder = File.join(File.dirname(archive_file_path), "moodle_test_export")
-    @exporter = Moodle::Converter.new(:export_archive_path=>archive_file_path, :course_name=>'oi', :base_download_dir=>unzipped_file_path)
+    @converter = Moodle::Converter.new(:export_archive_path=>archive_file_path, :course_name=>'oi', :base_download_dir=>unzipped_file_path)
     @converter.export
     @course_data = @converter.course.with_indifferent_access
     @converter.delete_unzipped_archive
@@ -21,18 +21,26 @@ describe Moodle::Converter do
     @cm = ContentMigration.create(:context => @course)
   end
 
-  # you can test by importing individual types
   it "should convert discussion topics" do
     DiscussionTopic.process_migration(@course_data, @cm)
     @cm.migration_settings[:warnings].should be_nil
-    @course.discussion_topics.count.should == 6
+    @course.discussion_topics.count.should == 1
 
-    dt = @course.discussion_topics.find_by_migration_id('33')
-    dt.title.should == "Topic title"
-    dt.message.should == "No message"
+    dt = @course.discussion_topics.first
+    dt.title.should == "News forum"
+    dt.message.should == "<p>General news and announcements</p>"
   end
 
-  # Or you can import the whole course
+  it "should convert assignments" do
+    Assignment.process_migration(@course_data, @cm)
+    @cm.migration_settings[:warnings].should be_nil
+    @course.assignments.count.should == 5
+
+    assignment = @course.assignments[0]
+    assignment.title.should == "Create a Rails site"
+    assignment.description.should == "<p>Use `rails new` to create your first Rails site</p>"
+  end
+
   it "should successfully import the course" do
     @course.import_from_migration(@course_data, nil, @cm)
 
