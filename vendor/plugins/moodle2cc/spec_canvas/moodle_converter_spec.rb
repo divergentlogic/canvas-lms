35c55b3aa4c9b18d94f07afe2bfe4317e1632dcf
Christopher Durtschi <christopher.durtschi@gmail.com>
2012-07-14 07:09:33 -0600

    More quiz specs
    Moodle Choice -> Canvas Quiz spec still failing

--- vendor/plugins/moodle2cc/spec_canvas/moodle_converter_spec.rb
+++ vendor/plugins/moodle2cc/spec_canvas/moodle_converter_spec.rb
@@ -20,74 +20,188 @@ describe Moodle::Converter do
     @cm = ContentMigration.create(:context => @course)
   end
 
-  it "should convert discussion topics" do
-    DiscussionTopic.process_migration(@course_data, @cm)
-    @cm.migration_settings[:warnings].should be_nil
-    @course.discussion_topics.count.should == 1
-
-    dt = @course.discussion_topics.first
-    dt.title.should == "News forum"
-    dt.message.should == "<p>General news and announcements</p>"
-  end
-
-  it "should convert assignments" do
-    Assignment.process_migration(@course_data, @cm)
-    @cm.migration_settings[:warnings].should be_nil
-    @course.assignments.count.should == 5
-
-    assignment = @course.assignments[0]
-    assignment.title.should == "Create a Rails site"
-    assignment.description.should == "<p>Use `rails new` to create your first Rails site</p>"
-  end
-
-  it "should convert wikis" do
-    # must run twice for link references
-    WikiPage.process_migration(@course_data, @cm)
-    WikiPage.process_migration(@course_data, @cm)
-
-    @cm.migration_settings[:warnings].should be_nil
-    wiki = @course.wiki
-    wiki.should_not be_nil
-    wiki.wiki_pages.count.should == 3
-
-    page = wiki.wiki_pages[0]
-    page.title.should == "My Wiki"
-    page.url.should == 'my-wiki-my-wiki'
-    html = Nokogiri::HTML(page.body)
-    href = html.search('a').first.attributes['href'].value
-    href.should == "/courses/#{@course.id}/wiki/my-wiki-link"
-
-    page = wiki.wiki_pages[1]
-    page.title.should == "link"
-    page.url.should == 'my-wiki-link'
-
-    page = wiki.wiki_pages[2]
-    page.title.should == "New Wiki"
-    page.url.should == 'new-wiki-new-wiki'
-  end
-
-  it "should convert quizzes" do
-    @course.import_from_migration(@course_data, nil, @cm)
-    @course.quizzes.count.should == 3
-  end
-
-  it "should convert Moodle Choice module to a quiz" do
-    @course.import_from_migration(@course_data, nil, @cm)
-    quiz = @course.quizzes.find_by_title "My Choice"
-    quiz.quiz_questions.count.should == 1
-    question = quiz.quiz_questions.first
-    question.question_data[:question_name].should == "My Choice"
-    question.question_data[:question_text].should == "What is your choice?"
-  end
-
   it "should successfully import the course" do
     @course.import_from_migration(@course_data, nil, @cm)
-
-    # everything should be imported
-
-    # This is slower though. You can put it in the before(:all) but that doesn't
-    # clean up after itself, so that's annoying
-    # You can try to that in the before(:each), if it's not too slow that'll work great
+    @cm.migration_settings[:warnings].should be_nil
   end
 
+  context "discussion topics" do
+    before(:each) do
+      #These need to be ran twice because they can reference each other
+      DiscussionTopic.process_migration(@course_data, @cm)
+      DiscussionTopic.process_migration(@course_data, @cm)
+    end
+
+    it "should convert discussion topics" do
+      @course.discussion_topics.count.should == 1
+
+      dt = @course.discussion_topics.first
+      dt.title.should == "News forum"
+      dt.message.should == "<p>General news and announcements</p>"
+    end
+  end
+
+  context "assignments" do
+    before(:each) do
+      #These need to be ran twice because they can reference each other
+      Assignment.process_migration(@course_data, @cm)
+      Assignment.process_migration(@course_data, @cm)
+    end
+
+    it "should convert assignments" do
+      @course.assignments.count.should == 5
+
+      assignment = @course.assignments.find_by_title 'Create a Rails site'
+      assignment.should_not be_nil
+      assignment.description.should == "<p>Use `rails new` to create your first Rails site</p>"
+    end
+  end
+
+  context "wiki pages" do
+    before(:each) do
+      #These need to be ran twice because they can reference each other
+      WikiPage.process_migration(@course_data, @cm)
+      WikiPage.process_migration(@course_data, @cm)
+    end
+
+    it "should convert wikis" do
+      wiki = @course.wiki
+      wiki.should_not be_nil
+      wiki.wiki_pages.count.should == 3
+
+      page = wiki.wiki_pages.find_by_title 'My Wiki'
+      page.should_not be_nil
+      page.url.should == 'my-wiki-my-wiki'
+      html = Nokogiri::HTML(page.body)
+      href = html.search('a').first.attributes['href'].value
+      href.should == "/courses/#{@course.id}/wiki/my-wiki-link"
+
+      page = wiki.wiki_pages.find_by_title 'link'
+      page.should_not be_nil
+      page.url.should == 'my-wiki-link'
+
+      page = wiki.wiki_pages.find_by_title 'New Wiki'
+      page.should_not be_nil
+      page.url.should == 'new-wiki-new-wiki'
+    end
+  end
+
+  context "quizzes" do
+    before(:each) do
+      question_data = AssessmentQuestion.process_migration(@course_data, @cm)
+      Group.process_migration(@course_data, @cm)
+      LearningOutcome.process_migration(@course_data, @cm)
+      Rubric.process_migration(@course_data, @cm)
+      AssignmentGroup.process_migration(@course_data, @cm)
+      ExternalFeed.process_migration(@course_data, @cm)
+      GradingStandard.process_migration(@course_data, @cm)
+      Quiz.process_migration(@course_data, @cm, question_data)
+    end
+
+    it "should convert quizzes" do
+      @course.quizzes.count.should == 3
+    end
+
+    it "should convert Moodle Quiz module to a quiz" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      quiz.should_not be_nil
+      #quiz.description.should match /Pop quiz hot shot/
+      quiz.quiz_questions.count.should == 9
+    end
+
+    it "should convert Moodle Calculated Question to Canvas calculated_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[0]
+      question.question_data[:question_name].should == "Calculated Question"
+      question.question_data[:question_text].should == "How much is [a] + [b] ?"
+      question.question_data[:question_type].should == 'calculated_question'
+      question.question_data[:neutral_comments].should == 'Calculated Question General Feedback'
+    end
+
+    it "should convert Moodle Description Question to Canvas text_only_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[1]
+      question.question_data[:question_name].should == "Description Question"
+      question.question_data[:question_text].should == "Description Question Text"
+      question.question_data[:question_type].should == 'text_only_question'
+    end
+
+    it "should convert Moodle Essay Question to Canvas essay_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[2]
+      question.question_data[:question_name].should == "Essay Question"
+      question.question_data[:question_text].should == "Essay Question Text"
+      question.question_data[:question_type].should == 'essay_question'
+      question.question_data[:neutral_comments].should == 'Essay Question General Feedback'
+    end
+
+    it "should convert Moodle Matching Question to Canvas matching_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[3]
+      question.question_data[:question_name].should == "Matching Question"
+      question.question_data[:question_text].should == "Matching Question Text"
+      question.question_data[:question_type].should == 'matching_question'
+      question.question_data[:neutral_comments].should == 'Matching Question General Feedback'
+    end
+
+    it "should convert Moodle Embedded Answers Question to Canvas essay_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[4]
+      question.question_data[:question_name].should == "Embedded Answers Question"
+      question.question_data[:question_text].should match /Embedded Answers Question Text/
+      question.question_data[:question_type].should == 'essay_question'
+      question.question_data[:neutral_comments].should == 'Embedded Answers Question General Feedback'
+    end
+
+    it "should convert Moodle Multiple Choice Question to Canvas multiple_choice_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[5]
+      question.question_data[:question_name].should == "Multiple Choice Question"
+      question.question_data[:question_text].should == "Multiple Choice Question Text"
+      question.question_data[:question_type].should == 'multiple_choice_question'
+      question.question_data[:neutral_comments].should == 'Multiple Choice Question General Feedback'
+    end
+
+    it "should convert Moodle Numerical Question to Canvas numerical_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[6]
+      question.question_data[:question_name].should == "Numerical Question"
+      question.question_data[:question_text].should == "Numerical Question Text"
+      question.question_data[:question_type].should == 'numerical_question'
+      question.question_data[:neutral_comments].should == 'Numerical Question General Feedback'
+    end
+
+    it "should convert Moodle Short Answer Question to Canvas short_answer_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[7]
+      question.question_data[:question_name].should == "Short Answer Question"
+      question.question_data[:question_text].should == "Short Answer Question Text"
+      question.question_data[:question_type].should == 'short_answer_question'
+      question.question_data[:neutral_comments].should == 'Short Answer Question General Feedback'
+    end
+
+    it "should convert Moodle True/False Question to Canvas true_false_question" do
+      quiz = @course.quizzes.find_by_title "First Quiz"
+      question = quiz.quiz_questions[8]
+      question.question_data[:question_name].should == "True or False Question"
+      question.question_data[:question_text].should == "True or False Question Text"
+      question.question_data[:question_type].should == 'true_false_question'
+      question.question_data[:neutral_comments].should == 'True or False Question General Feedback'
+    end
+
+    it "should convert Moodle Questionnaire module to a quiz" do
+      quiz = @course.quizzes.find_by_title "My Questionnaire"
+      quiz.should_not be_nil
+      quiz.quiz_questions.count.should == 10
+    end
+
+    it "should convert Moodle Choice module to a quiz" do
+      quiz = @course.quizzes.find_by_title "My Choice"
+      quiz.should_not be_nil
+      quiz.quiz_questions.count.should == 1
+      question = quiz.quiz_questions.first
+      question.question_data[:question_name].should == "My Choice"
+      question.question_data[:question_text].should == "What is your choice?"
+    end
+  end
 end
